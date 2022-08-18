359f0399dc340a4e04d1ee01eecb04a90acfc70b
Pechenka121212 <pechenie121212@mail.ru>
2022-08-18 13:47:13 +0300

    Added the ability to edit tasks. Closed #9

--- modules/core/core.cpp
+++ modules/core/core.cpp
@@ -11,9 +11,15 @@ core::Task::Task()
 
 core::Task::Task(unsigned int* priority, char* text, bool* isDone)
 {
-	_priority = priority;
+	_priority = new unsigned int;
 
-	_text = text;
+	*_priority = *priority;
+
+	Helper helper;
+
+	_text = new char[*helper.LengthCharText(text)];
+
+	helper.CopyText(text, _text);
 
 	_isDone = isDone;
 }
@@ -40,4 +46,37 @@ char* core::Task::GetText()
 bool* core::Task::GetIsDone()
 {
 	return _isDone;
+}
+
+unsigned int* core::Helper::LengthCharText(char* text)
+{
+	unsigned int* lengthText = new unsigned int;
+
+	*lengthText = 0;
+
+	while(*(text + *lengthText) != '\0')
+	{
+		*lengthText += 1;
+	}
+
+	return lengthText;
+}
+
+void core::Helper::CopyText(char* inputText, char* resultText)
+{
+	unsigned int* lengthText = LengthCharText(inputText);
+
+	for(int i = 0; i < *lengthText; i++)
+	{
+		*(resultText + i) = *(inputText + i);
+	}
+}
+
+void controller::TaskController::EditTask(core::Task* editableTask, unsigned int* newPriority, char* newText, bool* newIsDone)
+{
+	editableTask->core::Task::~Task();
+
+	core::Task* editedTask = new core::Task(newPriority, newText, newIsDone);
+
+	editableTask = editedTask;
 }
